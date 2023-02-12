527aa513f2c3d356b9031933ff17345b59a1e3f5
bhavik-goplani <56516858+bhavik-goplani@users.noreply.github.com>
2023-02-11 21:52:47 -0600

    express boilerplate added

---
+++ server/routes/users.js
@@ -0,0 +1,5 @@
+const express = require('express')
+const router = express.Router()
+router.use(logger)
+
+module.exports = router
---
+++ server/server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+app.use(logger)
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
+
+const userRouter = require('./routes/users')
+app.use('/users', userRouter)
+app.listen(3000)
