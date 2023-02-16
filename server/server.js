568ee1e5e27aca8939c432f4a0bf199fa6644fe8
bhavik-goplani <56516858+bhavik-goplani@users.noreply.github.com>
2023-02-15 22:24:56 -0800

    Mongo connection established

--- server/server.js
+++ server/server.js
@@ -1,10 +1,27 @@
 const express = require('express')
+const cors = require("cors")
+const bodyParser = require("body-parser")
+
 const app = express()
-app.use(logger)
-app.set('view engine', 'ejs')
+
+require("dotenv").config({ path: "./config.env" })
+const port = process.env.PORT || 3000
+app.use(cors())
+app.use(express.json())
+app.use(bodyParser.json())
+app.use(require("./routes/record"))
 app.use(express.static('public'))
 app.use(express.urlencoded({extended: true}))
 
+// get driver connection
+const dbo = require("./db/conn");
+
 const userRouter = require('./routes/users')
 app.use('/users', userRouter)
-app.listen(3000)
+
+app.listen(port, () => {
+    dbo.connectToServer(function (err) {
+        if (err) console.error(err)
+    })
+    console.log(`Server is running on port: ${port}`)
+})
