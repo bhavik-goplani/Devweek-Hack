fd927ffa72bcb2f1fce09147dd9872f13291e4f6
hudsonhrh <hudsonhrh7@gmail.com>
2023-02-16 11:39:34 -0800

    gpt api test

--- server/routes/gpt/gpt.js
+++ server/routes/gpt/gpt.js
@@ -1,3 +1,22 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const app = express()
+const port = 3000
+
+app.use(bodyParser.json())
+app.use(cors())
+
+app.get('/', (req,res) =>{
+    res.send("hello world")
+
+})
+
+app.listen(port, ()=> {
+
+    console.log("example app listening")
+})
+
 
 const OpenAI = require('openai')
 const {Configuration, OpenAIApi} = OpenAI
@@ -7,4 +26,9 @@ const configuration = new Configuration({
     organization: "org-CtbyLQVWPne6XC9sQfuMqGNf",
     apiKey: process.env.GPT,
 });
+
 const openai = new OpenAIApi(configuration);
+
+
+const router = express.Router()
+module.exports = router
