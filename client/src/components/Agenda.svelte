68740c824a79a7449c98060b1edf03eeb1cdeb6f
Emma Nasseri <enasseri02@gmail.com>
2023-02-17 03:38:27 -0600

    testing json input to treeview

--- client/src/components/Agenda.svelte
+++ client/src/components/Agenda.svelte
@@ -1,51 +1,52 @@
 <script>
-  import { Treeview, Button, TextField } from "smelte";
- 
+  import { Treeview, TextField } from "smelte";
+  let data = [
+    {
+      text: "Topic 1",
+      items: [
+        { text: "Detail 1" },
+        { text: "Detail 2" },
+        { text: "Detail 3" },
+        {
+          text: "subtest4",
+          items: [
+            { text: "Detail 1" },
+            { text: "Detail 2" },
+            { text: "Detail 3" },
+            { text: "Detail 4" },
+          ],
+        },
+      ],
+    },
+    {
+      text: "Topic 2",
+      items: [
+        { text: "Detail 1" },
+        { text: "Detail 2" },
+        { text: "Detail 3" },
+        { text: "Detail 4" },
+      ],
+    },
+  ];
+
+  let inputData = "";
 </script>
 
+<p>{inputData}</p>
 <div class="tree-wrapper">
-    <h6 class="mb-3 mt-6">JSON for testing</h6>
-    <TextField label="Test label" textarea rows="5" outlined />
-  <Treeview
-    items={[
-      {
-        text: "Topic 1",
-        items: [
-          { text: "Detail 1" },
-          { text: "Detail 2" },
-          { text: "Detail 3" },
-          {
-            text: "subtest4",
-            items: [
-              { text: "Detail 1" },
-              { text: "Detail 2" },
-              { text: "Detail 3" },
-              { text: "Detail 4" },
-            ],
-          },
-        ],
-      },
-      {
-        text: "Topic 2",
-        items: [
-          { text: "Detail 1" },
-          { text: "Detail 2" },
-          { text: "Detail 3" },
-          { text: "Detail 4" },
-        ],
-      },
-      {
-        text: "Topic 3",
-        items: [
-          { text: "subtest" },
-          { text: "subtest2" },
-          { text: "subtest3" },
-          { text: "subtest4" },
-        ],
-      },
-    ]}
+  <h6 class="mb-3 mt-6">JSON for testing</h6>
+  <TextField
+    label="Test label"
+    textarea
+    rows="5"
+    outlined
+    bind:value={inputData}
+   
   />
+  <Treeview items={inputData} />
 </div>
+
+
 
 <style>
   .tree-wrapper {
