767ff7d3df8b42ac517e4c968e51cb3da5e566b9
bhavik-goplani <56516858+bhavik-goplani@users.noreply.github.com>
2023-02-17 03:39:00 -0800

    notes archive getData and postData function

--- client/src/pages/NotesArchive.svelte
+++ client/src/pages/NotesArchive.svelte
@@ -7,43 +7,34 @@
     const dispatchermodule = createEventDispatcher();
 
     
-    async function Backend()
+    async function getData()
     {
       console.log("Sexy Smartness")
-      fetch('https://localhost:3000/users')
+      fetch('http://localhost:3000/users')
           .then(response => {            
-            console.log(response);
-          })
-          .then(data => {
-            console.log(data);
+            console.log(JSON.parse(response.body));
           })
           .catch(error => {
             console.log(error)
           });
-
-      const request = await fetch("https://localhost:3000/users",{
-        method:'POST',
-        headers:{
-          'Content-Type':'application/json'
-        },
-
-        body: JSON.stringify({
-          agenda1:[{
-            heading:"Dummy Heading",
-            currentcontent:"Zoom api docs are mid"
-          }]
-        })
-
-        
-      }).then(res =>{
-        return res.json
-      }).then(data =>{
-        console.log(data.json)
-      }).catch(err =>console.log(err))
     }
 
-
-    
+    async function postData () {
+      const response = await fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          agenda1:{
+            heading:"Dummy Heading",
+            currentcontent:"Zoom api docs are mid"
+          }
+        })
+      }).then (res => {
+        console.log(res.body)
+      }).catch(err => console.log(err))
+    }
   </script>
   
 
@@ -52,8 +43,8 @@
 	<h1>Notes Archive</h1>
     <p>This page should display a users' notes.</p>
 
-    <Button on:click={Backend}>Try Backend</Button>
-
+    <Button on:click={getData}>Get Data</Button>
+    <Button on:click={postData}>Post Data</Button>
     //ZoomAPI.transcript = agenda1
 	
 </main>
