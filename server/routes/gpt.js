6837d15868d5087198af5061aa3aac964c9d6ee4
hudsonhrh <hudsonhrh7@gmail.com>
2023-02-16 19:00:58 -0800

    added action item creation for full text

--- server/routes/gpt.js
+++ server/routes/gpt.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const router = express.Router()
 
-const agendaType = "main"
+const agendaType = "full"
 const subheading = "Rundown"
-const text = "today we are going to go over the failed launch of our DAO and our goals for next week. im excited about the future of the DAO project"
-
+const textMain = "today we are going to go over the failed launch of our DAO and our goals for next week. im excited about the future of the DAO project"
+const textFull= "[Scrum Master]: Good morning everyone! Let's get started with our daily stand-up. [Developer 1]: Hey there! I'm currently working on the new feature for our app. Hoping to finish it up today. [Developer 2]: I'm working on fixing some bugs in the login flow. It's been a bit tricky, but I'm making progress. [Developer 3]: I just finished up the design for the new landing page. I'll be starting on the front-end implementation later today. [Scrum Master]: Great updates, everyone. Is there anything blocking your progress? [Developer 1]: Not really, just waiting on some feedback from the product owner. [Developer 2]: I could use some help from the back-end team to make sure my fixes don't cause any issues elsewhere. [Developer 3]: No blockers for me right now. [Scrum Master]: Thanks for the updates. Let's make sure to communicate any issues or blockers so we can stay on track. Have a productive day!"
 
 
 router.get('/', async (req,res) =>{
@@ -13,7 +13,21 @@ router.get('/', async (req,res) =>{
     if(agendaType == "main"){
         const response = await openai.createCompletion({
             model: "text-davinci-003",
-            prompt: "Generate bullet point list to summarize the discussion under meeting agenda heading " + subheading+ "for this text: "+ text,
+            prompt: "Generate bullet point list to summarize the discussion under meeting agenda heading " + subheading+ "for this text: "+ textMain,
+            max_tokens:1000,
+            
+        })
+        console.log(response.data)
+
+        if (response.data.choices[0].text){
+            res.json({message: response.data.choices[0].text})
+        }
+    
+    }
+    if(agendaType == "full"){
+        const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: "Generate bullet point action items from the meeting with names for who in the company needs to do it " + subheading+ "for this text: "+ textFull,
             max_tokens:1000,
             
         })
